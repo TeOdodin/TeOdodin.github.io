5110f56a46461e16bff8c113f505d4cffcae9691
Théo DAUDIN <axblaze8@gmail.com>
2025-02-27 01:34:26 +0100

    update cosplay-page preview

--- cosplay-page/expandable-container.js
+++ cosplay-page/expandable-container.js
@@ -1,9 +1,17 @@
-const expandables = document.getElementsByClassName("expandable");
+const expandables = document.querySelectorAll(".expandable");
 
 for(let parent of expandables) {
-  const content = parent.getElementsByClassName("content")[0];
+  const content = parent.querySelector(".content");
   parent.children[0].onclick = () => {
     parent.classList.toggle("expanded");
     content.style.maxHeight = parent.classList.contains('expanded') ? content.scrollHeight + 'px' : '0';
   }
 };
+
+const legalNotesButtton = document.querySelector("#mentions_legales_btn");
+const legalNotesContainer = document.querySelector("#mentions_legales");
+const toggleLegalNotes = () => {
+  legalNotesContainer.style.display = legalNotesContainer.style.display !== 'block' ? 'block' : 'none';
+};
+legalNotesButtton.onclick = toggleLegalNotes;
+legalNotesContainer.onclick = toggleLegalNotes;
